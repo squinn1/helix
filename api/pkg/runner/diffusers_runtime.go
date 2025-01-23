235f80781aa2d7de21eacd8f19e50179b181b774
Phil Winder <phil@winder.ai>
2025-01-23 15:59:53 +0000

    missing url

--- api/pkg/runner/diffusers_runtime.go
+++ api/pkg/runner/diffusers_runtime.go
@@ -268,16 +268,18 @@ type HTTPDoer interface {
 
 type DiffusersClient struct {
 	client HTTPDoer
+	url    string
 }
 
 func NewDiffusersClient(ctx context.Context, url string) (*DiffusersClient, error) {
 	return &DiffusersClient{
 		client: http.DefaultClient,
+		url:    url,
 	}, nil
 }
 
 func (d *DiffusersClient) Healthz(ctx context.Context) error {
-	req, err := http.NewRequestWithContext(ctx, "GET", "/healthz", nil)
+	req, err := http.NewRequestWithContext(ctx, "GET", d.url+"/healthz", nil)
 	if err != nil {
 		return err
 	}
@@ -296,7 +298,7 @@ type DiffusersVersionResponse struct {
 }
 
 func (d *DiffusersClient) Version(ctx context.Context) (string, error) {
-	req, err := http.NewRequestWithContext(ctx, "GET", "/version", nil)
+	req, err := http.NewRequestWithContext(ctx, "GET", d.url+"/version", nil)
 	if err != nil {
 		return "", err
 	}
@@ -330,7 +332,7 @@ func (d *DiffusersClient) Pull(ctx context.Context, modelName string) error {
 	if err != nil {
 		return err
 	}
-	req, err := http.NewRequestWithContext(ctx, "POST", "/pull", bytes.NewBuffer(body))
+	req, err := http.NewRequestWithContext(ctx, "POST", d.url+"/pull", bytes.NewBuffer(body))
 	if err != nil {
 		return err
 	}
@@ -356,7 +358,7 @@ func (d *DiffusersClient) Warm(ctx context.Context, modelName string) error {
 	if err != nil {
 		return err
 	}
-	req, err := http.NewRequestWithContext(ctx, "POST", "/warm", bytes.NewBuffer(body))
+	req, err := http.NewRequestWithContext(ctx, "POST", d.url+"/warm", bytes.NewBuffer(body))
 	if err != nil {
 		return err
 	}
